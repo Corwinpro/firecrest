07991606543754f6baf4e74a4e82a0b44a6ccc2e
corwinpro <corwinat@gmail.com>
2019-04-08 22:22:03 +0100

    demo geometry updated

--- demos/simple_geometry.py
+++ demos/simple_geometry.py
@@ -1,5 +1,5 @@
-from firecrest.mesh.boundaryelement import BSplineElement
-from firecrest.mesh.geometry import SimplyConnected
+from firecrest.mesh.boundaryelement import BSplineElement, LineElement
+from firecrest.mesh.geometry import SimpleDomain
 
 control_points = [
     [0.0, 0.0],
@@ -9,8 +9,6 @@ control_points = [
     [0.4, 0.0],
     [0.5, 0.1],
 ]
-boundary2 = BSplineElement(
-    "type", [[0.5, 0.1], [0.5, -0.2], [0.0, -0.2], [0.0, 0.0]], degree=1
-)
+boundary2 = LineElement("type", [[0.5, 0.1], [0.5, -0.2], [0.0, -0.2], [0.0, 0.0]])
 boundary1 = BSplineElement("type", control_points)
-domain = SimplyConnected([boundary1, boundary2])
+domain = SimpleDomain([boundary1, boundary2])
