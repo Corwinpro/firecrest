9dd4cc23e0b84e3c4eb8d7e86d123df75964ab94
corwinpro <corwinat@gmail.com>
2019-04-16 14:31:06 +0100

    minor changes in structure

--- firecrest/fem/base_fspace.py
+++ firecrest/fem/base_fspace.py
@@ -2,14 +2,25 @@ import dolfin as dolf
 
 
 class BaseFunctionSpace:
+    """
+    Base Function Space factory.
+    params:
+        - domain: geometrical domain with dolfin mesh
+        - spaces: collection of Space objects with Finite Element type, polynomial order, space dimension
+    """
+
     def __init__(self, domain, spaces):
         self.domain = domain
         self.spaces = spaces
         self.function_spaces = self.generate_function_spaces()
 
     def generate_function_spaces(self, **kwargs):
+        """
+        Generates a dolfin FunctionSpace of either single Finite Element (if only Space one provided)
+        or Mixed Elements (if more the one Space provided).
+        """
         elements = [
-            self._generate_function_space(space, **kwargs) for space in self.spaces
+            self._generate_finite_element(space, **kwargs) for space in self.spaces
         ]
         if len(elements) == 1:
             return dolf.FunctionSpace(self.domain.mesh, elements[0], **kwargs)
@@ -17,17 +28,17 @@ class BaseFunctionSpace:
         mixed_element = dolf.MixedElement(elements)
         return dolf.FunctionSpace(self.domain.mesh, mixed_element, **kwargs)
 
-    def _generate_function_space(self, space, **kwargs):
+    def _generate_finite_element(self, space, **kwargs):
+        """
+        Generates Finite Element for given Space.
+        """
         cell = self.domain.mesh.ufl_cell()
         element_type = space.element_type or "Lagrange"
-        return self.make_element(element_type, cell, space.order, space.dimension)
 
-    @staticmethod
-    def make_element(el_type, cell, order, dimension):
-        if dimension == 1 or dimension == 0 or dimension == "scalar":
-            return dolf.FiniteElement(el_type, cell, order)
-        elif dimension == "vector":
+        if space.dimension == 1 or space.dimension == 0 or space.dimension == "scalar":
+            return dolf.FiniteElement(element_type, cell, space.order)
+        elif space.dimension == "vector":
             dimension = cell.geometric_dimension()
-            return dolf.VectorElement(el_type, cell, order, dimension)
-        elif dimension >= 2:
-            return dolf.VectorElement(el_type, cell, order, dimension)
+            return dolf.VectorElement(element_type, cell, space.order, dimension)
+        elif space.dimension >= 2:
+            return dolf.VectorElement(element_type, cell, space.order, space.dimension)
