6c1581d72f61a78165ec5fcdec5865b96a3ce6c5
corwinpro <corwinat@gmail.com>
2019-04-18 15:00:53 +0100

    weak forms

--- firecrest/fem/base_weakform.py
+++ firecrest/fem/base_weakform.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
+import dolfin as dolf
 
 
 class BaseWeakForm(ABC):
-    pass
-
-        
+    def __init__(self, domain, **kwargs):
+        self.domain = domain
+        self.geometric_dimension = self.domain.mesh.ufl_cell().geometric_dimension()
+        self.I = dolf.Identity(self.geometric_dimension)
+        self.dirichlet_bcs = []
--- firecrest/fem/tv_acoustic_weakform.py
+++ firecrest/fem/tv_acoustic_weakform.py
@@ -7,8 +7,8 @@ import ufl
 
 class TVAcousticWeakForm(BaseWeakForm):
     def __init__(self, domain, **kwargs):
-        self.domain = domain
-        self.function_space = TVAcousticFunctionSpace(self.domain).function_spaces
+        super().__init__(domain, **kwargs)
+        self.function_space_factory = TVAcousticFunctionSpace(self.domain)
         self.trial_functions = dolf.TrialFunctions(self.function_space)
         self.test_functions = dolf.TestFunctions(self.function_space)
 
@@ -18,7 +18,37 @@ class TVAcousticWeakForm(BaseWeakForm):
         )
 
         self.dolf_constants = self.get_constants(kwargs)
-        self.I = dolf.Identity(self.domain.mesh.ufl_cell().geometric_dimension())
+        self.allowed_stress_bcs = {
+            "noslip": "Dirichlet",
+            "inflow": "Dirichlet",
+            "free": "Neumann",
+            "force": "Neumann",
+            "impedance": "Robin",
+        }
+
+        self.allowed_temperature_bcs = {
+            "isothermal": "Dirichlet",
+            "temperature": "Dirichlet",
+            "adiabatic": "Neumann",
+            "heat_flux": "Neumann",
+            "thermal_accomodation": "Robin",
+        }
+
+    @property
+    def function_space(self):
+        return self.function_space_factory.function_spaces
+
+    @property
+    def pressure_function_space(self):
+        return self.function_space_factory.pressure_function_space
+
+    @property
+    def velocity_function_space(self):
+        return self.function_space_factory.velocity_function_space
+
+    @property
+    def temperature_function_space(self):
+        return self.function_space_factory.temperature_function_space
 
     def get_constants(self, kwargs):
         self._gamma = kwargs.get("gamma", 1.4)
@@ -89,7 +119,9 @@ class TVAcousticWeakForm(BaseWeakForm):
         momementum_component = dolf.inner(velocity, test_velocity)
         energy_component = self.entropy(pressure, temperature) * test_temperature
 
-        return continuity_component + momementum_component + energy_component
+        return (
+            continuity_component + momementum_component + energy_component
+        ) * dolf.dx
 
     def spatial_component(self, trial=None, test=None):
         if trial is None:
@@ -110,4 +142,127 @@ class TVAcousticWeakForm(BaseWeakForm):
             dolf.grad(test_temperature), self.heat_flux(temperature)
         )
 
-        return continuity_component + momementum_component + energy_component
+        return (
+            continuity_component + momementum_component + energy_component
+        ) * dolf.dx
+
+    def boundary_components(self, trial=None, test=None):
+        """
+        I suppose this is the place where I actually consider
+        various boundary conditions and specific values for them.
+        For instance, if I have a boundary element with prescribed
+        velocity profile on it, I must a priory know this is a DirichletBC,
+        and if I have a heat flux (Neumann) boundary condition on a wall,
+        it must be hard-coded as a part of a weak form, not DirichletBC.
+
+        I expect the usage should be something like:
+            bcond = {"noslip" : True, "heat_flux" : 1.}
+            bcond = {}
+        """
+        if trial is None:
+            trial = self.trial_functions
+        if test is None:
+            test = self.test_functions
+        _, velocity, temperature = trial
+        _, test_velocity, test_temperature = test
+
+        stress_boundary_component = dolf.Constant(0.0) * self.domain.ds
+        temperature_boundary_component = dolf.Constant(0.0) * self.domain.ds
+        dirichlet_bcs = []
+
+        for boundary in self.domain.boundary_elements:
+            # Step 1. Parse boundary condition data provided by boundary elements.
+            # We only accept one boundary condition for stress/velocity and temperature/heat flux.
+            temperature_bc = self._verify_boundary_condition(
+                boundary.bcond, self.allowed_temperature_bcs
+            )
+            stress_bc = self._verify_boundary_condition(
+                boundary.bcond, self.allowed_stress_bcs
+            )
+            temperature_bc_type = self.allowed_temperature_bcs[temperature_bc]
+            stress_bc_type = self.allowed_stress_bcs[temperature_bc]
+            # Step 2. If the boundary condition is one of the Dirichlet-compatible,
+            # we construct Dirichlet boundary condition.
+            if temperature_bc_type == "Dirichlet":
+                if temperature_bc == "isothermal":
+                    temperature = dolf.Constant(0.0)
+                elif temperature_bc == "temperature":
+                    temperature = dolf.Constant(boundary.bcond[temperature_bc])
+                else:
+                    raise TypeError(
+                        f"Invalid temperature boundary condition type for {temperature_bc_type} condition."
+                    )
+                dirichlet_bcs.append(
+                    dolf.DirichletBC(
+                        self.temperature_function_space,
+                        temperature,
+                        self.domain.ds,
+                        boundary.surface_index,
+                    )
+                )
+
+            if stress_bc_type == "Dirichlet":
+                if stress_bc == "noslip":
+                    stress = dolf.Constant((0.0,) * self.geometric_dimension)
+                elif stress_bc == "inflow":
+                    stress = dolf.Constant(boundary.bcond[temperature_bc])
+                else:
+                    raise TypeError(
+                        f"Invalid temperature boundary condition type for {stress_bc_type} condition."
+                    )
+                dirichlet_bcs.append(
+                    dolf.DirichletBC(
+                        self.velocity_function_space,
+                        stress,
+                        self.domain.ds,
+                        boundary.surface_index,
+                    )
+                )
+
+            # Step 3. If the boundary condition is one of the Neumann or Robin,
+            # we construct necessary boundary integrals in weak form.
+            if temperature_bc_type == "Neumann" or temperature_bc_type == "Robin":
+                if temperature_bc == "adiabatic":
+                    heat_flux = dolf.Constant(0.0)
+                elif temperature_bc == "heat_flux":
+                    heat_flux = dolf.Constant(boundary.bcond[temperature_bc])
+                elif temperature_bc == "thermal_accomodation":
+                    heat_flux = (
+                        -dolf.Constant(boundary.bcond[temperature_bc]) * temperature
+                    )
+                else:
+                    raise TypeError(
+                        f"Invalid temperature boundary condition type for {temperature_bc_type} condition."
+                    )
+                temperature_boundary_component += (
+                    -heat_flux
+                    * test_temperature
+                    * self.domain.ds((boundary.surface_index,))
+                )
+
+            if stress_bc_type == "Neumann" or stress_bc_type == "Robin":
+                if stress_bc == "free":
+                    stress = dolf.Constant((0.0,) * self.geometric_dimension)
+                elif stress_bc == "force":
+                    stress = dolf.Constant(boundary.bcond[stress_bc])
+                elif stress_bc == "impedance":
+                    stress = dolf.Constant(boundary.bcond[stress_bc]) * velocity
+                else:
+                    raise TypeError(
+                        f"Invalid temperature boundary condition type for {stress_bc_type} condition."
+                    )
+                stress_boundary_component += -dolf.inner(
+                    stress, test_velocity
+                ) * self.domain.ds((boundary.surface_index,))
+
+        return dirichlet_bcs, stress_boundary_component, temperature_boundary_component
+
+    @staticmethod
+    def _verify_boundary_condition(bcond, allowed_bconds):
+        bc = allowed_bconds.intersection(bcond)
+        if len(bc) != 1:
+            raise TypeError(
+                "Incorrect number of temperature-related boundary condition."
+                f"One expected, {len(bc)} received."
+            )
+        return bc
