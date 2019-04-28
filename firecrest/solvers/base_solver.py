82a8118a4141bb33e42eef4ffb52a12864839fb0
corwinpro <corwinat@gmail.com>
2019-04-28 16:40:01 +0100

    First commits for unsteady tv_acoustic solver

--- firecrest/solvers/base_solver.py
+++ firecrest/solvers/base_solver.py
@@ -1,30 +1,14 @@
 from abc import ABC, abstractmethod
 import dolfin as dolf
 
+LOG_LEVEL = 30
+
 
 class BaseSolver(ABC):
     def __init__(self, domain, **kwargs):
+        dolf.set_log_level(LOG_LEVEL)
         self.domain = domain
-        self.mesh = self.domain.mesh
-        self.boundaries = self.domain.boundary_elements
-        self.n = dolf.FacetNormal(self.mesh)
-
-    def dirichlet_boundary_condition(self, function_space, boundary):
-        return dolf.DirichletBC(
-            function_space, boundary.bcond, self.domain.ds, boundary.surface_index
-        )
-
-    def ds(self, boundary_type=None, boundary=None):
-        return self.domain.get_boundary_measure(boundary_type, boundary)
-
-    def dx(self):
-        return self.domain.dx
 
     @abstractmethod
-    def solve(self):
-        pass
-
-
-class TestSolver(BaseSolver):
     def solve(self):
         pass
---
+++ firecrest/solvers/tv_acoustic_solver.py
@@ -0,0 +1,68 @@
+from firecrest.solvers.base_solver import BaseSolver
+from firecrest.fem.tv_acoustic_weakform import TVAcousticWeakForm
+import dolfin as dolf
+import logging
+
+DEFAULT_DT = 1.0e-3
+
+
+class TVAcousticSolver(BaseSolver):
+    def __init__(self, domain, **kwargs):
+        super().__init__(domain)
+        self.forms = TVAcousticWeakForm(domain, **kwargs)
+        self._dt = kwargs.get("dt", DEFAULT_DT)
+        self._reverse_dt = dolf.Constant(1.0 / self._dt)
+
+    def set_initial_state(self, initial_state):
+        self.initial_state = initial_state
+
+    def implicit_euler(self, initial_state):
+        temporal_component = self.forms.temporal_component()
+        temporal_component_old = self.forms.temporal_component(initial_state)
+
+        spatial_component = self.forms.spatial_component()
+        dirichlet_bcs, stress_boundary_component, temperature_boundary_component = (
+            self.forms.boundary_components()
+        )
+        form = (
+            self._reverse_dt * (temporal_component - temporal_component_old)
+            + spatial_component
+            + stress_boundary_component
+            + temperature_boundary_component
+        )
+        return form, dirichlet_bcs
+
+    def implicit_crank_nicolson(self, initial_state):
+        temporal_component = self.forms.temporal_component()
+        temporal_component_old = self.forms.temporal_component(initial_state)
+
+        spatial_component = self.forms.spatial_component()
+        dirichlet_bcs, stress_boundary_component, temperature_boundary_component = (
+            self.forms.boundary_components()
+        )
+        spatial_component_old = self.forms.spatial_component(initial_state)
+        _, stress_boundary_component_old, temperature_boundary_component_old = self.forms.boundary_components(
+            initial_state
+        )
+        form = (
+            self._reverse_dt * (temporal_component - temporal_component_old)
+            + dolf.Constant(0.5) * (spatial_component + spatial_component_old)
+            + dolf.Constant(0.5)
+            * (stress_boundary_component + stress_boundary_component_old)
+            + dolf.Constant(0.5)
+            * (temperature_boundary_component + temperature_boundary_component_old)
+        )
+        return form, dirichlet_bcs
+
+    def solve(self, initial_state, time_scheme="implicit_euler"):
+        if time_scheme == "implicit_euler":
+            form, bcs = self.implicit_euler(initial_state)
+        elif time_scheme == "implicit_crank_nicolson":
+            form, bcs = self.implicit_crank_nicolson(initial_state)
+        else:
+            raise NotImplementedError(
+                f"Time discretization scheme {time_scheme} is not yet implemented."
+            )
+        w = dolf.Function(self.forms.function_space)
+        dolf.solve(dolf.lhs(form) == dolf.rhs(form), w, bcs)
+        return w
