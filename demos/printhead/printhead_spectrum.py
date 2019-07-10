28c6c175abfc218f3f5bebdd96a29e1fa6816f32
corwinpro <corwinat@gmail.com>
2019-07-10 15:54:21 +0100

    Updated spectrum demo: plot uI and uR, and the spectrum plot

--- demos/printhead/printhead_spectrum.py
+++ demos/printhead/printhead_spectrum.py
@@ -2,14 +2,14 @@ from firecrest.mesh.boundaryelement import LineElement
 from firecrest.mesh.geometry import SimpleDomain
 from firecrest.solvers.eigenvalue_tv_acoustic_solver import EigenvalueTVAcousticSolver
 import dolfin as dolf
-
+import matplotlib.pyplot as plt
 
 control_points_1 = [[9.75, -0.2], [9.75, 0.0], [0.0, 0.0], [0.0, 3.0]]
 control_points_2 = [[0.0, 3.0], [1.0, 3.0]]
 control_points_3 = [[1.0, 3.0], [1.0, 1.0], [10.0, 1.0]]
 control_points_4 = [[10.0, 1.0], [10.0, -0.2], [9.75, -0.2]]
 
-el_size = 0.1
+el_size = 0.05
 boundary1 = LineElement(
     control_points_1, el_size=el_size, bcond={"noslip": True, "isothermal": True}
 )
@@ -26,15 +26,25 @@ domain_boundaries = (boundary1, boundary2, boundary3, boundary4)
 domain = SimpleDomain(domain_boundaries)
 
 solver = EigenvalueTVAcousticSolver(
-    domain, complex_shift=0.0 + 0.15j, Re=100.0, Pe=1.0, nmodes=4
+    domain, complex_shift=-0.02 + 0.5j, Re=1000.0, Pe=1.0, nmodes=10
 )
 solver.solve()
 
-file = dolf.File("mode.pvd")
-for i in range(solver.nof_modes_converged):
-    ev, real_mode, imag_mode = solver.restore_eigenfunction(i)
-    real_mode[1].rename("u", "u")
-    file << real_mode[1], i
+spectrum = []
+mode_imag = dolf.File("mode_imag.pvd")
+mode_real = dolf.File("mode_real.pvd")
 
+for i in range(int(solver.nof_modes_converged / 2)):
+    ev, real_mode, imag_mode = solver.extract_solution(i)
+    spectrum.append(ev)
+    imag_mode[1].rename("uI", "uI")
+    real_mode[1].rename("uR", "uR")
+    mode_real << real_mode[1], i
+    mode_imag << imag_mode[1], i
+
+
+plt.plot([ev.real for ev in spectrum], [ev.imag for ev in spectrum], "o")
+plt.grid(True)
+plt.show()
 # file << real_mode[1], 0
 # file << imag_mode[1], 1
