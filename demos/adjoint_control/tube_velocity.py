3e9627fb3989d6474e0726a23c8f1bb4cac19d8f
corwinpro <corwinat@gmail.com>
2019-08-12 20:23:09 +0100

    Reworked with new TimeSeries implementation

--- demos/adjoint_control/tube_velocity.py
+++ demos/adjoint_control/tube_velocity.py
@@ -66,28 +66,54 @@ final_state = tuple(final_state)
 
 adjoint_history = solver.solve_adjoint(final_state, verbose=True)
 
-adjoint_stress = [solver.forms.stress(adjoint_history[time][0], adjoint_history[time][1]) for time in adjoint_history]
-adjoint_stress_averaged = [
-    dolf.assemble(
-        dolf.dot(dolf.dot(stress, solver.domain.n), solver.domain.n)
+# adjoint_stress = [
+#     solver.forms.stress(adjoint_history[time][0], adjoint_history[time][1])
+#     for time in adjoint_history
+# ]
+
+# adjoint_stress = StateHistory.from_dict(
+#     {
+#         time: solver.forms.stress(adjoint_history[time][0], adjoint_history[time][1])
+#         for time in adjoint_history
+#     }
+# )
+adjoint_stress = adjoint_history.apply(lambda x: solver.forms.stress(x[0], x[1]))
+
+# To statehistory (Timeseries?)
+# adjoint_stress_averaged = [
+#     dolf.assemble(
+#         dolf.dot(dolf.dot(stress, solver.domain.n), solver.domain.n)
+#         * solver.domain.ds((boundary4.surface_index,))
+#     )
+#     for stress in adjoint_stress
+# ]
+adjoint_stress_averaged = adjoint_stress.apply(
+    lambda x: dolf.assemble(
+        dolf.dot(dolf.dot(x, solver.domain.n), solver.domain.n)
         * solver.domain.ds((boundary4.surface_index,))
     )
-    for stress in adjoint_stress
-]
+)
 
 dU = 1.0e-3
+# delta_energy = (
+#     solver._dt
+#     * dU
+#     * (
+#         sum(adjoint_stress_averaged)
+#         - 0.5 * adjoint_stress_averaged[-1]
+#         # - 0.5 * adjoint_stress_averaged[0]
+#     )
+# )
+# print(delta_energy)
 delta_energy = (
     solver._dt
     * dU
-    * (
-        sum(adjoint_stress_averaged)
-        - 0.5 * adjoint_stress_averaged[-1]
-        # - 0.5 * adjoint_stress_averaged[0]
-    )
+    * (sum(adjoint_stress_averaged.values()) - 0.5 * adjoint_stress_averaged.recent)
 )
 print(delta_energy)
 # print(solver._dt * dU * (sum(adjoint_stress_averaged)) / 2.)
 
+# Add: summation of two time series
 
 for i in range(1, 11):
     boundary4.bcond["inflow"] = NormalInflow(1.0 - i * dU)
