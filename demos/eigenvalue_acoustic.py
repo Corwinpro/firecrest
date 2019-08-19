ea57307928cf8f3a5a8ad9faed6745556f8132fd
corwinpro <corwinat@gmail.com>
2019-08-19 17:26:49 +0100

    testing impedance stuff

--- demos/eigenvalue_acoustic.py
+++ demos/eigenvalue_acoustic.py
@@ -7,25 +7,45 @@ import dolfin as dolf
 control_points_1 = [[0.0, 1.0], [0.0, 0.0], [1.0, 0.0]]
 control_points_2 = [[1.0, 0.0], [1.0, 1.0], [0.0, 1.0]]
 boundary1 = LineElement(
-    control_points_1, el_size=0.05, bcond={"noslip": True, "isothermal": True}
+    control_points_1, el_size=0.03, bcond={"noslip": True, "isothermal": True}
 )
 boundary2 = LineElement(
-    control_points_2, el_size=0.05, bcond={"free": True, "adiabatic": True}
+    control_points_2, el_size=0.03, bcond={"impedance": 1.0, "adiabatic": True}
 )
 domain_boundaries = (boundary1, boundary2)
 domain = SimpleDomain(domain_boundaries)
 
 
-solver = EigenvalueTVAcousticSolver(domain, complex_shift=0 + 2j, Re=100.0, Pe=1.0)
+solver = EigenvalueTVAcousticSolver(domain, complex_shift=-0.3 + 3.0j, Re=500.0, Pr=1.0)
 solver.solve()
 
-ev, real_mode, imag_mode = solver.restore_eigenfunction(0)
+mode_imag = dolf.File("mode_imag.pvd")
+mode_real = dolf.File("mode_real.pvd")
 
-file = dolf.File("mode.pvd")
-file << real_mode[1], 0
-file << imag_mode[1], 1
 
-func = dolf.Function(solver.forms.function_space)
-# dolf.assign(func.sub(0), real_mode[0]) # This works
-dolf.assign(func, list(real_mode + imag_mode))
-file << func.sub(4), 2
+def impedance_check(mode):
+    return normal_velocity(mode) - dolf.assemble(
+        -dolf.dot(
+            dolf.dot(solver.forms.stress(mode[0], mode[1]), solver.domain.n),
+            solver.domain.n,
+        )
+        * solver.domain.ds(boundary2.surface_index)
+    )
+
+
+def normal_velocity(mode):
+    return dolf.assemble(
+        (dolf.dot(mode[1], solver.domain.n)) * solver.domain.ds(boundary2.surface_index)
+    )
+
+
+for i in range(int(solver.nof_modes_converged / 2)):
+    ev, real_mode, imag_mode = solver.extract_solution(i)
+    imag_mode[1].rename("uI", "uI")
+    real_mode[1].rename("uR", "uR")
+    mode_real << real_mode[1], i
+    mode_imag << imag_mode[1], i
+    print("Z*un - sigma_n_n real: ", impedance_check(real_mode))
+    print("Z*un - sigma_n_n imag: ", impedance_check(imag_mode))
+    print("un real:", normal_velocity(real_mode))
+    print("un imag:", normal_velocity(imag_mode))
