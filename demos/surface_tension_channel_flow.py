f83e34e1bc570d5da1461d76e06d96651adcdf6a
corwinpro <corwinat@gmail.com>
2019-09-06 11:35:40 +0100

    Changed geometry to printhead

--- demos/surface_tension_channel_flow.py
+++ demos/surface_tension_channel_flow.py
@@ -2,9 +2,34 @@ from firecrest.mesh.boundaryelement import LineElement
 from firecrest.mesh.geometry import SimpleDomain
 from firecrest.solvers.unsteady_tv_acoustic_solver import UnsteadyTVAcousticSolver
 from firecrest.models.free_surface_cap import SurfaceModel
-
 import dolfin as dolf
 from collections import namedtuple
+
+elsize = 0.08
+height = 0.7
+length = 10.0
+offset_top = 1.0
+nozzle_r = 0.1
+nozzle_l = 0.2
+nozzle_offset = 0.2
+
+# Define constants
+L = 1.0e-4
+c_s = 1.0e3
+rho = 1.0e3
+epsilon = 1.0e-3
+gamma_st = 50.0e-3
+mu = 1.0e-2
+Re = rho * c_s * L / mu
+a1 = 4.0 / 3.0
+a2 = 8
+kappa_prime = epsilon / nozzle_r ** 4 * 0.25  # up to a constant
+
+# nondimensional
+alpha_1 = nozzle_l / (3.1415 * nozzle_r ** 2) * a1
+alpha_2 = nozzle_l / (3.1415 * nozzle_r ** 3) / Re * a2 / nozzle_r
+gamma_nd = 2 * gamma_st / (rho * c_s ** 2 * nozzle_r * epsilon)
+
 
 Constants = namedtuple(
     "Constants",
@@ -43,52 +68,103 @@ nondim_constants = Constants(
 current_time = 0.0
 
 
-surface_model = SurfaceModel(nondim_constants)
+surface_model = SurfaceModel(nondim_constants, kappa_t0=0.25)
 
-control_points_1 = [[0.0, 0.0], [1.0e-16, 1.0]]
-control_points_2 = [[1.0e-16, 1.0], [0.2, 1.0 - 1.0e-16]]
-control_points_3 = [[0.2, 1.0 - 1.0e-16], [0.2, 1.0e-16]]
-control_points_4 = [[0.2, 1.0e-16], [0.0, 0.0]]
+control_points_1 = [[offset_top, height], [1.0e-16, height]]
+control_points_free_left = [[1.0e-16, height], [0.0, 0.0 + 1.0e-16]]
+control_points_bot_left = [
+    [0.0, 0.0 + 1.0e-16],
+    [length / 2.0 - nozzle_r - nozzle_offset, 0.0],
+]
+control_points_refine_left = [
+    [length / 2.0 - nozzle_r - nozzle_offset, 0.0],
+    [length / 2.0 - nozzle_r, 0.0],
+    [length / 2.0 - nozzle_r, -nozzle_l],
+]
+control_points_2 = [
+    [length / 2.0 - nozzle_r, -nozzle_l],
+    [length / 2.0 + nozzle_r, -nozzle_l + 1.0e-16],
+]
+control_points_refine_right = [
+    [length / 2.0 + nozzle_r, -nozzle_l + 1.0e-16],
+    [length / 2.0 + nozzle_r, 0.0],
+    [length / 2.0 + nozzle_r + nozzle_offset, 0.0],
+]
+control_points_bot_right = [
+    [length / 2.0 + nozzle_r + nozzle_offset, 0.0],
+    [length, 0.0 + 1.0e-16],
+]
+control_points_free_right = [[length, 0.0 + 1.0e-16], [length, height]]
+control_points_3 = [[length, height], [length - offset_top, height + 1.0e-16]]
+control_points_4 = [[length - offset_top, height + 1.0e-16], [offset_top, height]]
 
-el_size = 0.005
 
 boundary1 = LineElement(
-    control_points_1, el_size=el_size, bcond={"noslip": True, "adiabatic": True}
+    control_points_1, el_size=elsize, bcond={"noslip": True, "adiabatic": True}
+)
+boundary_free_left = LineElement(
+    control_points_free_left, el_size=elsize, bcond={"free": True, "adiabatic": True}
+)
+boundary_bot_left = LineElement(
+    control_points_bot_left, el_size=elsize, bcond={"noslip": True, "adiabatic": True}
+)
+boundary_refine_left = LineElement(
+    control_points_refine_left,
+    el_size=elsize / 4.0,
+    bcond={"noslip": True, "adiabatic": True},
 )
 boundary2 = LineElement(
-    control_points_2, el_size=el_size, bcond={"free": True, "adiabatic": True}
-)
-boundary3 = LineElement(
-    control_points_3, el_size=el_size, bcond={"noslip": True, "adiabatic": True}
-)
-boundary4 = LineElement(
-    control_points_4,
-    el_size=el_size,
+    control_points_2,
+    el_size=elsize / 4.0,
     bcond={"normal_force": surface_model, "adiabatic": True},
 )
-domain_boundaries = (boundary1, boundary2, boundary3, boundary4)
+boundary_refine_right = LineElement(
+    control_points_refine_right,
+    el_size=elsize / 4.0,
+    bcond={"noslip": True, "adiabatic": True},
+)
+boundary_bot_right = LineElement(
+    control_points_bot_right, el_size=elsize, bcond={"noslip": True, "adiabatic": True}
+)
+boundary_free_right = LineElement(
+    control_points_free_right, el_size=elsize, bcond={"free": True, "adiabatic": True}
+)
+boundary3 = LineElement(
+    control_points_3, el_size=elsize, bcond={"noslip": True, "adiabatic": True}
+)
+boundary4 = LineElement(
+    control_points_4, el_size=elsize, bcond={"noslip": True, "adiabatic": True}
+)
+domain_boundaries = (
+    boundary1,
+    boundary_free_left,
+    boundary_bot_left,
+    boundary_refine_left,
+    boundary2,
+    boundary_refine_right,
+    boundary_bot_right,
+    boundary_free_right,
+    boundary3,
+    boundary4,
+)
 domain = SimpleDomain(domain_boundaries)
 
-solver = UnsteadyTVAcousticSolver(domain, Re=5.0e3, Pr=10.0, dt=1.0e-3)
+
+solver = UnsteadyTVAcousticSolver(domain, Re=5.0e3, Pr=10.0, dt=4.0e-3)
 initial_state = (0.0, (0.0, 0.0), 0.0)
 
-f = dolf.File("temp.pvd")
-P = dolf.Function(solver.forms.pressure_function_space.collapse())
-P.rename("P", "P")
-# P.interpolate(initial_state[0])
-# f << P
 
-for i in range(1000):
+for i in range(10000):
     old_state = initial_state
     w = solver.solve(initial_state)
 
     initial_state = w.split(True)
-    if i % 10 == 9:
+    if i % 20 == 9:
         solver.output_field(initial_state)
 
     # Updating the curvature
     flow_rate = dolf.assemble(
-        dolf.inner(initial_state[1], domain.n) * domain.ds((boundary4.surface_index,))
+        dolf.inner(initial_state[1], domain.n) * domain.ds((boundary2.surface_index,))
     )
     surface_model.update_curvature(flow_rate, solver._dt)
 
