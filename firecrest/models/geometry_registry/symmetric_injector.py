d3cbf7166474820d6b7f7f6857d0dadc346adea8
corwinpro <corwinat@gmail.com>
2020-01-17 20:19:47 +0000

    MAINT: symmetric injector class refactored to allow zero slip region near the actuator

--- firecrest/models/geometry_registry/symmetric_injector.py
+++ firecrest/models/geometry_registry/symmetric_injector.py
@@ -6,17 +6,13 @@ class SymmetricInjectorAssembler(BaseAssembler):
         channel_height = self.geometry_data["channel_height"]
         width = self.geometry_data["channel_width"]
         actuator_length = self.geometry_data["actuator_length"]
+        actuator_slip = self.geometry_data.get("actuator_slip", 0.0)
         nozzle_r = self.geometry_data["nozzle_r"]
         nozzle_l = self.geometry_data["nozzle_l"]
         nozzle_offset = self.geometry_data["nozzle_offset"]
 
-        cp_slip_wall = [
-            [width - actuator_length, channel_height],
-            [width - actuator_length - 0.1, channel_height + 1.0e-16],
-        ]
-
         cp_noslip_wall = [
-            [width - actuator_length - 0.1, channel_height + 1.0e-16],
+            [width - actuator_length - actuator_slip, channel_height + 1.0e-16],
             [0.0, channel_height],
             [0.0, 0.0],
             [width - nozzle_r - nozzle_offset, 0.0],
@@ -38,44 +34,57 @@ class SymmetricInjectorAssembler(BaseAssembler):
             [width, channel_height + 1.0e-16],
             [width - actuator_length, channel_height],
         ]
-        return [
-            cp_slip_wall,
-            cp_noslip_wall,
-            cp_nozzle_noslip_wall,
-            cp_nozzle_free_surface,
-            cp_symmetry_plane,
-            cp_actuator,
-        ]
+
+        control_points = []
+
+        if actuator_slip > 0.0:
+            cp_slip_wall = [
+                [width - actuator_length, channel_height],
+                [width - actuator_length - actuator_slip, channel_height + 1.0e-16],
+            ]
+            control_points.append(cp_slip_wall)
+
+        control_points.extend(
+            [
+                cp_noslip_wall,
+                cp_nozzle_noslip_wall,
+                cp_nozzle_free_surface,
+                cp_symmetry_plane,
+                cp_actuator,
+            ]
+        )
+
+        return control_points
 
     def generate_boundary_metadata(self):
         outer_slip_data = self._generate_boundary_dict(
-            name="outer_slip_meta",
+            name="outer_slip",
             default_element_size=self.element_size,
             default_bcond={"slip": True, "adiabatic": True},
         )
 
         outer_noslip_data = self._generate_boundary_dict(
-            name="outer_noslip_meta",
-            default_element_size=self.element_size,
+            name="outer_noslip",
+            default_element_size=self.element_size / 2.0,
             default_bcond={"noslip": True, "adiabatic": True},
         )
         nozzle_noslip_data = self._generate_boundary_dict(
-            name="nozzle_noslip_meta",
+            name="nozzle_noslip",
             default_element_size=self.element_size / 8.0,
             default_bcond={"noslip": True, "adiabatic": True},
         )
         nozzle_free_data = self._generate_boundary_dict(
-            name="nozzle_free_meta",
+            name="nozzle_free",
             default_element_size=self.element_size / 8.0,
             default_bcond={"normal_force": None, "adiabatic": True},
         )
         symmetry_plane_data = self._generate_boundary_dict(
-            name="nozzle_free_meta",
-            default_element_size=self.element_size / 2.0,
+            name="symmetry_plane",
+            default_element_size=self.element_size,
             default_bcond={"slip": True, "adiabatic": True},
         )
         actuator_data = self._generate_boundary_dict(
-            name="actuator_meta",
+            name="actuator",
             default_element_size=self.element_size / 20.0,
             default_bcond={"inflow": (0.0, 0.0), "adiabatic": True},
         )
