c0989a4563f92bf68a6a80a33282752c5414113a
corwinpro <corwinat@gmail.com>
2020-01-19 17:43:55 +0000

    MAINT: explicit kwarg for maximum number of iterations in `minimize`

--- firecrest/misc/optimization_mixin.py
+++ firecrest/misc/optimization_mixin.py
@@ -50,14 +50,14 @@ class OptimizationMixin(ABC):
 
         return self.jacobian_cache[key] * self.default_renormalization
 
-    def minimize(self, x0, bnds):
+    def minimize(self, x0, bnds, maxiter=200):
         res = minimize(
             self.objective,
             x0,
             method=self.optimization_method,
             jac=self.jacobian,
             bounds=bnds,
-            options={"disp": True, "maxiter": 40, "ftol": 1.0e-8},
+            options={"disp": True, "maxiter": maxiter, "ftol": 1.0e-8},
         )
         return res
 
