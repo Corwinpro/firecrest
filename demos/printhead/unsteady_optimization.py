da89462b499e8a28ca4aad2843382a99d6ec5f0d
corwinpro <corwinat@gmail.com>
2020-01-23 22:18:07 +0000

    DEV: major refactor

--- demos/printhead/unsteady_optimization.py
+++ demos/printhead/unsteady_optimization.py
@@ -101,22 +101,22 @@ print(experiment_id)
 
 
 class NormalInflow:
-    def __init__(self, series: TimeSeries):
+    def __init__(self, control_data):
         self.counter = 1
-        self.series = list(series.values())
+        self.control_data = control_data
 
     def eval(self):
-        if self.counter < len(self.series):
-            value = (0.0, self.series[self.counter])
+        if self.counter < len(self.control_data):
+            value = (0.0, self.control_data[self.counter])
             self.counter += 1
             return value
         return 0.0, 0.0
 
 
-def pre(function):
+def discrete_to_continuous_control(function):
     def wrapped(self, discrete_control):
-        restored_control = self.linear_basis.extrapolate(list(discrete_control))
-        return function(self, restored_control)
+        continuous_control = self.linear_basis.extrapolate(list(discrete_control))
+        return function(self, continuous_control)
 
     return wrapped
 
@@ -140,71 +140,94 @@ def post(function):
     return wrapped
 
 
+def continuous_to_discrete_adj(function):
+    def wrapped(self, state):
+        continuous_gradient = function(self, state)
+        gradient_time_series = TimeSeries.from_list(
+            continuous_gradient, self.midpoint_grid
+        )
+        du = TimeSeries.interpolate_to_keys(gradient_time_series, self.time_grid)
+
+        discrete_grad = self.linear_basis.discretize(du.values())
+
+        print("gradient norm: ", (gradient_time_series * du).integrate())
+
+        print(
+            "discrete gradient norm: ",
+            (
+                gradient_time_series
+                * TimeSeries.from_list(
+                    self.linear_basis.extrapolate(discrete_grad), self.time_grid
+                )
+            ).integrate(),
+        )
+        return discrete_grad
+
+    return wrapped
+
+
 class OptimizationSolver(OptimizationMixin, UnsteadyTVAcousticSolver):
     def __init__(self, *args, signal_window=None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.linear_basis = PiecewiseLinearBasis(
-            np.array([float(key) for key in default_grid.keys()]),
-            width=signal_window,
-            reduced_basis=True,
-        )
+        self.signal_window = signal_window
+        self.initial_state = kwargs["initial_state"]
+        self.control_boundary = kwargs.get("control_boundary")
+        self.shared_boundary = kwargs.get("shared_boundary")
 
-    def flow_rate(self, state, boundary=shared_boundary):
+    def flow_rate(self, state, boundary):
         return 2.0 * dolf.assemble(
             dolf.inner(state[1], self.domain.n)
             * self.domain.ds((boundary.surface_index,))
         )
 
-    def _objective_state(self, control):
-        restored_control = self.linear_basis.extrapolate(list(control))
+    def initialize_control_boundary(self, control_data):
+        inflow = NormalInflow(control_data)
+        self.control_boundary.bcond["inflow"] = inflow
 
-        control_boundary.bcond["inflow"] = NormalInflow(
-            TimeSeries.from_list(restored_control, default_grid)
-        )
+    def log_intermediate_step(self, state, surface_model):
+        if energy_history_log_filename and run_mode == "single_run":
+            file_name = energy_history_log_filename + experiment_id + ".dat"
+            with open(file_name, "a") as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    [
+                        self.forms.energy(state),
+                        surface_model.surface_energy() / 2.0,
+                        self.forms.kinetic_energy_flux(
+                            state, (0.0, 1.0), control_boundary
+                        ),
+                        self.forms.kinetic_energy_flux(
+                            state, (0.0, -1.0), self.shared_boundary
+                        ),
+                        self.flow_rate(state, self.control_boundary),
+                        self.flow_rate(state, self.shared_boundary),
+                    ]
+                )
 
+    def initialize_shared_boundary(self):
         surface_model = SurfaceModel(nondim_constants, kappa_t0=initial_curvature)
-        if "normal_force" in shared_boundary.bcond:
-            shared_boundary.bcond["normal_force"] = surface_model
+        if "normal_force" in self.shared_boundary.bcond:
+            self.shared_boundary.bcond["normal_force"] = surface_model
+        return surface_model
+
+    @post
+    @discrete_to_continuous_control
+    def _objective_state(self, control):
+        self.initialize_control_boundary(control)
+
+        surface_model = self.initialize_shared_boundary()
 
         _old_flow_rate = 0
         for state in self.solve_direct(
-            initial_state, verbose=False, yield_state=True, plot_every=plot_every
+            self.initial_state, verbose=False, yield_state=True, plot_every=plot_every
         ):
-
-            if energy_history_log_filename and run_mode == "single_run":
-                file_name = energy_history_log_filename + experiment_id + ".dat"
-                with open(file_name, "a") as file:
-                    writer = csv.writer(file)
-                    writer.writerow(
-                        [
-                            self.forms.energy(state),
-                            surface_model.surface_energy() / 2.0,
-                            self.forms.kinetic_energy_flux(
-                                state, (0.0, 1.0), control_boundary
-                            ),
-                            self.forms.kinetic_energy_flux(
-                                state, (0.0, -1.0), shared_boundary
-                            ),
-                            self.flow_rate(state, control_boundary),
-                            self.flow_rate(state, shared_boundary),
-                        ]
-                    )
-
-            _new_flow_rate = self.flow_rate(state)
+            _new_flow_rate = self.flow_rate(state, self.shared_boundary)
             surface_model.update_curvature(
                 0.5 * (_new_flow_rate + _old_flow_rate), self._dt
             )
             _old_flow_rate = _new_flow_rate
 
-        if optimization_log_filename and run_mode == "optimization":
-            file_name = optimization_log_filename + experiment_id + ".dat"
-            output_data = [
-                self._objective((state, surface_model), verbose=False),
-                surface_model.kappa,
-            ] + list(control)
-            with open(file_name, "a") as file:
-                writer = csv.writer(file)
-                writer.writerow(output_data)
+            self.log_intermediate_step(state, surface_model)
 
         return state, surface_model
 
@@ -220,23 +243,23 @@ class OptimizationSolver(OptimizationMixin, UnsteadyTVAcousticSolver):
             )
         return acoustic_energy + free_energy
 
-    def boundary_averaged_stress(self, adjoint_state):
-        stress = self.forms.stress(adjoint_state[0], adjoint_state[1])
+    def boundary_averaged_stress(self, state, boundary):
+        stress = self.forms.stress(state[0], state[1])
         stress = dolf.assemble(
             dolf.dot(dolf.dot(stress, self.domain.n), self.domain.n)
-            * self.domain.ds((control_boundary.surface_index,))
+            * self.domain.ds((boundary.surface_index,))
         )
         return stress
 
+    @continuous_to_discrete_adj
     def _jacobian(self, state):
         state, surface_model = state
         state = (state[0], -state[1], state[2])
 
         adjoint_surface = AdjointSurfaceModel(direct_surface=surface_model)
-        shared_boundary.bcond["normal_force"] = adjoint_surface
+        self.shared_boundary.bcond["normal_force"] = adjoint_surface
 
-        adjoint_stress_averaged = midpoint_grid
-        current_time = self.timer["T"] - self.timer["dt"] / Decimal("2")
+        adjoint_stress_averaged = []
 
         _old_flow_rate = 0
         _new_flow_rate = 0
@@ -247,47 +270,54 @@ class OptimizationSolver(OptimizationMixin, UnsteadyTVAcousticSolver):
             initial_state=state, verbose=False, yield_state=True, plot_every=plot_every
         ):
             _old_flow_rate = _new_flow_rate
-            _new_flow_rate = self.flow_rate(adjoint_state)
+            _new_flow_rate = self.flow_rate(adjoint_state, self.shared_boundary)
             adjoint_surface.update_curvature(
                 0.5 * (_new_flow_rate + _old_flow_rate), self._dt
             )
 
-            adjoint_stress_averaged[current_time] = self.boundary_averaged_stress(
-                adjoint_state
+            adjoint_stress_averaged.append(
+                self.boundary_averaged_stress(adjoint_state, self.control_boundary)
             )
-            current_time -= self.timer["dt"]
 
-        du = TimeSeries.interpolate_to_keys(adjoint_stress_averaged, default_grid)
+        return adjoint_stress_averaged[::-1]
 
-        discrete_grad = self.linear_basis.discretize(du.values())
-
-        print("gradient norm: ", (adjoint_stress_averaged * du).integrate())
-
-        print(
-            "discrete gradient norm: ",
-            (
-                adjoint_stress_averaged
-                * TimeSeries.from_list(
-                    self.linear_basis.extrapolate(discrete_grad), default_grid
-                )
-            ).integrate(),
+    def run(self, initial_guess=None, bounds=None):
+        # 1. define control
+        # `time_grid` defines the time grid for the direct simulation
+        self.time_grid = TimeSeries.from_dict(
+            {
+                Decimal(k) * Decimal(self.timer["dt"]): 0
+                for k in range(int(self.timer["T"] / Decimal(self.timer["dt"])) + 1)
+            }
+        )
+        # `midpoint_grid` defines the time grid for the adjoint simulation
+        self.midpoint_grid = TimeSeries.from_dict(
+            {
+                Decimal(k + 0.5) * Decimal(timer["dt"]): 0
+                for k in range(int(timer["T"] / Decimal(timer["dt"])) - 1)
+            }
+        )
+        # We define a control space, a PiecewiseLinearBasis in this case
+        self.linear_basis = PiecewiseLinearBasis(
+            np.array([float(key) for key in self.time_grid.keys()]),
+            width=self.signal_window,
+            reduced_basis=True,
         )
 
-        return discrete_grad
+        # We prepare the initial guess and bounds on the control
+        if bounds is not None:
+            top_bound = [bounds[0] for _ in range(len(self.time_grid))]
+            low_bound = [bounds[1] for _ in range(len(self.time_grid))]
+            top_bound = self.linear_basis.discretize(top_bound)
+            low_bound = self.linear_basis.discretize(low_bound)
+            bounds = list(zip(low_bound, top_bound))
 
+        if initial_guess is None:
+            initial_guess = np.zeros(len(self.linear_basis.basis))
 
-default_grid = TimeSeries.from_dict(
-    {
-        Decimal(k) * Decimal(timer["dt"]): 0
-        for k in range(int(timer["T"] / Decimal(timer["dt"])) + 1)
-    }
-)
-midpoint_grid = TimeSeries.from_dict(
-    {
-        Decimal(k + 0.5) * Decimal(timer["dt"]): 0
-        for k in range(int(timer["T"] / Decimal(timer["dt"])) - 1)
-    }
-)
+        res = self.minimize(initial_guess, bounds)
+        return res
+
 
 solver = OptimizationSolver(
     domain,
@@ -297,36 +327,28 @@ solver = OptimizationSolver(
     signal_window=nondim_waveform_window,
     experiment_id=experiment_id,
     optimization_method=control_algorithm,
+    initial_state=(0.0, (0.0, 0.0), 0.0),
+    control_boundary=geometry_assembler.control_boundary,
+    shared_boundary=geometry_assembler.shared_boundary,
 )
-initial_state = (0.0, (0.0, 0.0), 0.0)
-
-
-x0 = [0.0 for _ in range(len(default_grid))]
-top_bound = [control_upper_limit for i in range(len(x0))]
-low_bound = [control_lower_limit for i in range(len(x0))]
-top_bound = solver.linear_basis.discretize(top_bound)
-low_bound = solver.linear_basis.discretize(low_bound)
-bnds = list(zip(low_bound, top_bound))
-if control_default_value:
-    x0 = control_default_value
-else:
-    x0 = solver.linear_basis.discretize(x0)
-
-if run_mode == "taylor_test":
-    x0 = [1.0e-4 for _ in range(len(x0))]
-    energy = []
-    _state = solver._objective_state(x0)
-    energy.append(solver._objective(_state))
-    print(energy[-1])
-    grad = np.array(solver._jacobian(_state))
-    x0 = np.array(x0)
-
-    for i in range(1, 11):
-        new_state = solver._objective_state(x0 + grad * 1.0e-6 * i)
-        energy.append(solver._objective(new_state))
-elif run_mode == "optimization":
-    res = solver.minimize(x0, bnds)
-elif run_mode == "single_run":
-    solver._objective_state(x0)
-else:
-    raise NotImplementedError(f"No mode called {run_mode} is implemented")
+res = solver.run(control_default_value, (control_lower_limit, control_upper_limit))
+#
+# if run_mode == "taylor_test":
+#     x0 = [1.0e-4 for _ in range(len(x0))]
+#     energy = []
+#     _state = solver._objective_state(x0)
+#     energy.append(solver._objective(_state))
+#     print(energy[-1])
+#     grad = np.array(solver._jacobian(_state))
+#     x0 = np.array(x0)
+#
+#     for i in range(1, 11):
+#         new_state = solver._objective_state(x0 + grad * 1.0e-6 * i)
+#         energy.append(solver._objective(new_state))
+# elif run_mode == "optimization":
+#     # res = solver.minimize(x0, bnds)
+#     res = solver.run(control_default_value, (control_lower_limit, control_upper_limit))
+# elif run_mode == "single_run":
+#     solver._objective_state(x0)
+# else:
+#     raise NotImplementedError(f"No mode called {run_mode} is implemented")
