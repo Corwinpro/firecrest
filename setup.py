ebc2eea61678cb1d49b467493703506f4d5b61ab
corwinpro <corwinat@gmail.com>
2021-10-09 17:08:45 +0100

    Change project requirements

--- setup.py
+++ setup.py
@@ -4,15 +4,19 @@ Setup configuration for installation via pip
 
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+
 # The find_packages function does a lot of the heavy lifting for us w.r.t.
 # discovering any Python packages we ship.
 setup(
     name="firecrest",
-    version="0.1dev",
+    version="0.1.0.dev0",
     packages=find_packages(),
     # PyPI packages required for the *installation* and usual running of the
     # tools.
-    install_requires=["numpy", "sympy", "scipy"],
+    install_requires=required,
     entry_points={"console_scripts": ["firecrest = firecrest.cli.firecrest:run"]},
     # Metadata for PyPI (https://pypi.python.org).
     description="Finite Element shape optimization tool based on FEniCS",
